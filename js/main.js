e3cc103f0b728af6684f46b4856d7acaaee4b196
sihlesawula <150780503+sihlesawula@users.noreply.github.com>
2025-01-07 13:49:33 +0200

    js update

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ let cartIcon = document.querySelector('#cart-icon')
 let cart = document.querySelector('.cart')
 let closeCart = document.querySelector('#close-cart')
 
+
+
 // Open Cart
 cartIcon.onclick = () => {
     cart.classList.add('active')
